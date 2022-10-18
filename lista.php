af7f19bf5a370ea1a0be04e2fdf162c168486288
Filipe <eduardojs999@gmail.com>
2022-10-18 10:13:08 -0300

    array

---
+++ lista.php
@@ -0,0 +1,8 @@
+<?php
+
+$umArray = [1,2,3];
+$outroArray = array(1,2,3);
+
+$primeiroIndice = $umArray[0];
+$segundoIndice = $umArray[1];
+$terceiroIndice = $umArray[2];
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = "Olá, mundo!";
+echo $idade;
+
+
+for ($i=0;$i<=100;$i++){
+    
+    if ($i%2==1){
+        echo $i, "\n";
+    }
+}
